3b86bcaab62601a24332c6bbc3b1bf50955db433
wilstermanz <4917@holbertonschool.com>
2022-05-20 10:19:01 -0700

    Fixed Betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,15 +8,14 @@
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n",
-		sizeof (char));
+			sizeof(char));
 	printf("Size of an int: %lu byte(s)\n",
-                sizeof (int));
+			sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n",
-                sizeof (long int));
+			sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n",
-                sizeof (long long int));
+			sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n",
-                sizeof (float));
-
+			sizeof(float));
 	return (0);
 }
