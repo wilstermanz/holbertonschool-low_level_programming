9527187f83b8bfc15a3c8df3a76a9d07c3104542
wilstermanz <4917@holbertonschool.com>
2022-05-23 12:21:16 -0700

    Solution for task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Identifies whether n is positive or negative.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 122; i >= 97; i--)
+		putchar(i);
+	putchar(10);
+	return (0);
+}
