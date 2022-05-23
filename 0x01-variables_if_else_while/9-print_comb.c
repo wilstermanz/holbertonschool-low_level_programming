3dba9f8f4b14977082ef479ab2dee91dc7cd1208
wilstermanz <4917@holbertonschool.com>
2022-05-23 12:52:39 -0700

    Solution for task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - The program begins here.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		if (i < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
