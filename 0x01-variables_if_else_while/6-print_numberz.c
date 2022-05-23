6fc7eb39aae160a5fccb9a7512f4b922689afbcd
wilstermanz <4917@holbertonschool.com>
2022-05-23 12:15:29 -0700

    Solution for task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Program begins here
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	putchar(10);
+	return (0);
+}
