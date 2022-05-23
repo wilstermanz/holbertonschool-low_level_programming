615792543de065c45fccce8429e59b64ad20c674
wilstermanz <4917@holbertonschool.com>
2022-05-23 11:50:15 -0700

    Solution for task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Identifies whether n is positive or negative.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 97; i <= 122; i++)
+		putchar(i);
+	for (i = 65; i <= 90; i++)
+		putchar(i);
+	putchar(10);
+	return (0);
+}
