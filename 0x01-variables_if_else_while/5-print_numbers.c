d48d79d01054f551b06fcd1e52db91ed15ce08d4
wilstermanz <4917@holbertonschool.com>
2022-05-23 12:11:16 -0700

    Solution for task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Program begins here
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%i", i);
+	printf("\n");
+	return (0);
+}
