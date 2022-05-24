b5bdf6447cbb79ed5be173c61ba5f87e00d829ca
wilstermanz <4917@holbertonschool.com>
2022-05-24 12:51:39 -0700

    Solution for task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day
+ * Return: No return
+ */
+
+void jack_bauer(void)
+{
+	int hh;
+	int mm;
+	
+	for (hh = 0; hh < 24; hh++)
+	{
+		for (mm = 0; mm < 60; mm++)
+		{
+			_putchar((hh / 10) + '0');
+			_putchar((hh % 10) + '0');
+			_putchar(58);
+			_putchar((mm / 10) + '0');
+			_putchar((mm % 10) + '0');
+			_putchar(10);
+		}
+	}
+}
