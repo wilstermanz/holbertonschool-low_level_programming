d094f4337fd138141a1ba97125b8867858af4c8b
wilstermanz <4917@holbertonschool.com>
2022-05-24 16:56:12 -0700

    Made Betty happy

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,7 @@ void times_table(void)
 {
 	int a;
 	int b;
-		
+
 	for (a = 0; a <= 9; a++)
 	{
 		_putchar(48);
@@ -24,7 +24,7 @@ void times_table(void)
 				_putchar((ab / 10) + '0');
 			else
 				_putchar(32);
-			
+
 			_putchar((ab % 10) + '0');
 
 			if (b < 9)
