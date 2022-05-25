7bde87f48816162068d7af9e4b6d589ff6c6fa28
wilstermanz <4917@holbertonschool.com>
2022-05-24 18:17:45 -0700

    Fixed return condition

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,9 +11,7 @@ void print_times_table(int n)
 	int a;
 	int b;
 
-	if ((n < 0) || (n > 15))
-		_putchar(0);
-	else
+	if ((n > 0) && (n < 15))
 	{
 		for (a = 0; a <= n; a++)
 		{
@@ -42,4 +40,6 @@ void print_times_table(int n)
 			_putchar('\n');
 		}
 	}
+	else
+		return;
 }
