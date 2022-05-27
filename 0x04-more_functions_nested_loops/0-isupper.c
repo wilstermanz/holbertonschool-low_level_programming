05308458f8387ca3b219e3a8464ea3532ff5e224
wilstermanz <4917@holbertonschool.com>
2022-05-27 09:10:24 -0700

    Solution for task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper - Find if input is a capital letter
+ * @c: input
+ * Return: 1 if true, else 0
+ */
+
+int _isupper(int c)
+{
+	int o;
+
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		o = 1;
+	}
+	else
+	{
+		o = 0;
+	}
+	
+	return (o);
+}
