a5cc4b8300774d297cce8fb25d145c9a760ddf1f
wilstermanz <4917@holbertonschool.com>
2022-05-27 12:24:04 -0700

    Solution for task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0-14 ten times
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		int ones = 48;
+		int tens = 49;
+
+		for (j = 0; j <= 14; j++)
+		{
+			if (j == 10)
+				ones = 48;
+			if (j >= 10)
+				_putchar(tens);
+			_putchar(ones);
+			ones++;
+		}
+		_putchar(10);
+	}
+}
