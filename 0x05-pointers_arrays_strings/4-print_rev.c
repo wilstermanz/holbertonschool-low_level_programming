ce1e6a5f09417fd5420560eff43c834d82d53a63
wilstermanz <4917@holbertonschool.com>
2022-06-01 14:36:11 -0700

    Solution for task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string input in reverse
+ * @s: String input
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i, j, count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	for (j = count; j >= 0; j--)
+		_putchar(s[j]);
+	_putchar(10);
+}
