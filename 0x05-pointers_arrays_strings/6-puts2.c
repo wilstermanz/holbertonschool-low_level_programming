14e139c9831fa62348072f7787544ad5276521ec
wilstermanz <4917@holbertonschool.com>
2022-06-01 15:41:36 -0700

    Solution for task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character in a string
+ * @str - input string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+	
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar(10);
+}
