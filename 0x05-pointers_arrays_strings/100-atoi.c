b01266cd5db2d78c78f35b5f1ddf6109ec65a090
wilstermanz <4917@holbertonschool.com>
2022-06-03 20:53:39 -0700

    Solution for task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/** _atoi - converts number in string to integer
+ * @s: input string
+ * Return: value of integer string
+ */
+
+int _atoi(char *s)
+{
+	int i, num, sign = 1, output = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			sign = sign * -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			output = output * 10;
+			num = (s[i] - '0');
+			output = output + num;
+		}
+		if ((s[i] >= '0' && s[i] <= '9') && (s[i + 1] < '0' || s[i + 1] > '9'))
+			break;
+	}
+	output = output * sign;
+	return (output);
+}
