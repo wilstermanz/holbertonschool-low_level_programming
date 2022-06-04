d33a3022bee4104a95e356c52a82b3e33212b8b0
wilstermanz <4917@holbertonschool.com>
2022-06-03 21:07:54 -0700

    Works for all int sizes

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,13 +16,21 @@ int _atoi(char *s)
 			sign = sign * -1;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			output = output * 10;
-			num = (s[i] - '0');
-			output = output + num;
+			if (sign == -1)
+			{
+				output = output * 10;
+				num = (s[i] - '0');
+				output = output - num;
+			}
+			else
+			{
+				output = output * 10;
+				num = (s[i] - '0');
+				output = output + num;
+			}
 		}
 		if ((s[i] >= '0' && s[i] <= '9') && (s[i + 1] < '0' || s[i + 1] > '9'))
 			break;
 	}
-	output = output * sign;
 	return (output);
 }
