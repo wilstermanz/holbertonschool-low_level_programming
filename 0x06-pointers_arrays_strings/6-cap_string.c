7cded9777119c0419ddfe3be9e39838769fbb410
wilstermanz <4917@holbertonschool.com>
2022-06-06 13:45:03 -0700

    Solution for task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * cap_string - Capitalizes first first letter of word in string
+ * @str: input string
+ * Return: pointer to string
+ */
+
+char *cap_string(char *str)
+{
+	int i, j;
+
+	char *sep = " \t\n,;.!?\"(){}";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			j = 0;
+			for (j = 0; sep[j] != '\0'; j++)
+			{
+				if (str[i - 1] == sep[j])
+					str[i] = str[i] - 32;
+			}
+		}
+	}
+
+	return (str);
+}
