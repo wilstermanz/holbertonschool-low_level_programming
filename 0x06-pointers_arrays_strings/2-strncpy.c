227e0856d7a68135da1ca4e0c49573139d31848e
wilstermanz <4917@holbertonschool.com>
2022-06-06 11:46:11 -0700

    Solution for task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strncopy - copies a string
+ * @dest: Destination string
+ * @src: string to be copied
+ * @n: number of bytes from src to be copied
+ * Return: pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+		if (src[i] == '\0')
+			break;
+	}
+	return (dest);
+}
