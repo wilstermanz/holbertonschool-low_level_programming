190b2df07347ee21e20f8045cf931aab98978e11
wilstermanz <4917@holbertonschool.com>
2022-06-06 12:45:21 -0700

    Solution for task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverses an array of integers
+ * @a - an array of integers
+ * @n - number of elements to swap
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+
+	for (i = 0; i = (n / 2); i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = tmp;
+	}
+}
