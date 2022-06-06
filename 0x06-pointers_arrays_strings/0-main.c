6171ecbd30b3a32ca025c4f3a127bc14c923c353
wilstermanz <4917@holbertonschool.com>
2022-06-06 11:22:16 -0700

    Created assignment directory, header, _putchar, and solution for task 0.

---
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    printf("s1 = ");
+    printf("%s\n", s1);
+    printf("s2 = ");
+    printf("%s", s2);
+    ptr = _strcat(s1, s2);
+    printf("s1 = ");
+    printf("%s", s1);
+    printf("s2 = ");
+    printf("%s", s2);
+    printf("ptr = ");
+    printf("%s", ptr);
+    return (0);
+}
