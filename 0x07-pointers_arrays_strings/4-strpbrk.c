40ccfb8143ec9739f563d2d9a862a2e2333923bd
wilstermanz <4917@holbertonschool.com>
2022-06-07 12:39:20 -0700

    fix for 4 - 8

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -15,15 +15,15 @@ char *_strpbrk(char *s, char *accept)
 	{
 		for (j = 0; *(accept + j); j++)
 		{
-			if (s[i] == accept[j])
+			if (*(s + i) == *(accept + j))
 			{
 				break;
 			}
 		}
-		if (accept[j] != '\0')
+		if (*(accept + j) != '\0')
 			break;
 	}
-	if (accept[j] != '\0')
+	if (*(accept + j) != '\0')
 		return (s + i);
 	else
 		return (0);
