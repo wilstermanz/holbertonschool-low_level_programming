f2b73032a95aafadaf49d5e0a6ab66651094a2ba
wilstermanz <4917@holbertonschool.com>
2022-06-07 07:59:22 -0700

    Solution for task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk - seaches a string for any set of bytes
+ * @s: input string to be searched
+ * @accept: string to be searched for
+ * Return: pointer to byte s that matches accept, or NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+			{
+				break;
+			}
+		}
+		if (accept[i] != '\0')
+			break;
+		s++;
+	}
+	return (s);
+}
