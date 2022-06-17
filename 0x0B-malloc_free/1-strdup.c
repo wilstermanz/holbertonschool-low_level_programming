870f219995671c3721f9fa3a057cf9e87c85bedf
wilstermanz <4917@holbertonschool.com>
2022-06-16 17:08:01 -0700

    Solution for task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a copy of a string
+ * @str: string to be copied
+ * Return: Pointer to str copy, or NULL if error
+ */
+
+char *_strdup(char *str)
+{
+	char *cpy;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	cpy = malloc(100);
+
+	for (i = 0; str[i]; i++)
+		cpy[i] = str[i];
+
+	return (cpy);
+}
