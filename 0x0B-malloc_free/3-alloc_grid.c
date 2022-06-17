952be85a1ea288110088976dbfecd37eacf9c90a
wilstermanz <4917@holbertonschool.com>
2022-06-16 19:18:30 -0700

    Changed height and width

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	grid = malloc(width * sizeof(int *));
+	grid = malloc(height * sizeof(int *));
 
 	if (grid == NULL)
 	{
@@ -23,16 +23,16 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-		grid[i] = malloc(height * sizeof(int));
-		if (grid == NULL)
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
 		{
 			free(grid);
 			return (NULL);
 		}
 
-		for (j = 0; j < height; j++)
+		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
 		}
