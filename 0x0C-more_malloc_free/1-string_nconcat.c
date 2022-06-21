387399afe4acaa55b92d6314a2d2b911811c8c37
wilstermanz <4917@holbertonschool.com>
2022-06-21 13:46:09 -0700

    Added junk

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,6 +28,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, j, size1, size2;
 	char *cat;
+	
+	char *junk;
+
+	junk = malloc(5);
+	free(junk);
 
 	if (s1 == NULL)
 		s1 = "";
