617c894c39a9f9cf24d01e1214866bcd5904f057
wilstermanz <4917@holbertonschool.com>
2022-06-21 13:20:22 -0700

    Checking the checker

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+unsigned int _strlen(char *s);
 
 /**
  * string_nconcat - concatenates two strings
@@ -11,34 +12,47 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int size, i, j;
-	char *out;
+	char *cat;
+	unsigned int i, count = 0;
+	unsigned int len;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	
-	size = 0;
-	while (s1[size])
-		size++;
 
-	out = malloc(sizeof(*out) * size + n + 1);
+	len = _strlen(s2);
+	if (_strlen(s2) > n)
+		len = n;
 
-	if (out == NULL)
+	cat = malloc(sizeof(char) * (_strlen(s1) + len + 1));
+
+	if (cat == NULL)
 		return (NULL);
 
-	for (i = 0, j = 0; i < (size + n); i++)
-	{
-		if (i < size)
-		{
-			out[i] = s1[i];
-		}
-		else
-		{
-			out[i] = s2[j++];
-		}
-	}
-	out[i] = '\0';
-	return (out);
+	for (; *s1; s1++, count++)
+		cat[count] = *s1;
+
+	for (i = 0; *s2 && i < n; i++, s2++, count++)
+		cat[count] = *s2;
+
+	cat[count] = '\0';
+
+	return (cat);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to first character of string
+ *
+ * Return: length of string
+ */
+unsigned int _strlen(char *s)
+{
+	int i = 0;
+
+	for (; *s != '\0'; s++)
+		i++;
+
+	return (i);
 }
