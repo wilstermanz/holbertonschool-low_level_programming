a0503a5cc3a7f8213e4fa49db4ec6c11465cc433
wilstermanz <4917@holbertonschool.com>
2022-06-21 10:47:08 -0700

    Solution for task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	array = malloc(sizeof(nmemb));
+	array = malloc(nmemb * sizeof(int *));
 	if (array == NULL)
 	{
 		free(array);
