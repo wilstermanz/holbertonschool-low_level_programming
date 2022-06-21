6f0151cde9186d959d1056439a04a160b2a576c6
wilstermanz <4917@holbertonschool.com>
2022-06-21 13:43:34 -0700

    Rolled back to previous version

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,10 +28,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, j, size1, size2;
 	char *cat;
-	char *junk;
-	
-	junk = malloc(5);
-	free(junk);
 
 	if (s1 == NULL)
 		s1 = "";
