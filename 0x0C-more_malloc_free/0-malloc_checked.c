84ddbdc9ca5115a1689da7a9dc2e31c0428041bf
wilstermanz <4917@holbertonschool.com>
2022-06-21 09:08:58 -0700

    Solution for task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Runs malloc and checks for errors
+ * @b: int to malloc
+ * Return: Returns pointer to allocated memory, or 98 if failure
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
+
