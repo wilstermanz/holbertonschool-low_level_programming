0ba22290aa604bd56fbe373b0063777c9dff240f
wilstermanz <4917@holbertonschool.com>
2022-06-22 08:49:32 -0700

    Solution for task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A structure for a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of dog's owners
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
