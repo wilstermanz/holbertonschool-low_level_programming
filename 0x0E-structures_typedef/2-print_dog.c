9bdab5fded992272b72bef3bfe6d0d478136086e
wilstermanz <4917@holbertonschool.com>
2022-06-22 10:32:48 -0700

    Added case for NULL input

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,10 @@
 
 void print_dog(struct dog *d)
 {
-
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
-
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
+	}
 }
