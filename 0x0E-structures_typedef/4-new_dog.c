b3f6a645415ec4c85a125d2bb731f36ae977a182
wilstermanz <4917@holbertonschool.com>
2022-06-22 10:52:38 -0700

    Created copies of name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,22 +12,33 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	/* declarations */
 	dog_t *my_dog;
+	char *nameCopy, *ownerCopy;
 
+	/* Check for NULL input */
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	/* Allocate space and check for errors */
 	my_dog = malloc(sizeof(dog_t));
-
 	if (my_dog == NULL)
 		return (NULL);
 
-	if (name == NULL)
+	nameCopy = malloc(sizeof(name));
+	if (nameCopy == NULL)
 		return (NULL);
 
-	if (owner == NULL)
+	ownerCopy = malloc(sizeof(owner));
+	if (ownerCopy == NULL)
 		return (NULL);
 
-	my_dog->name = name;
+	/* Set values */
+	nameCopy = name;
+	ownerCopy = owner;
+	my_dog->name = nameCopy;
 	my_dog->age = age;
-	my_dog->owner = owner;
+	my_dog->owner = ownerCopy;
 
 	return (my_dog);
 }
