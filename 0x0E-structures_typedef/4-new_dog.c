e3c6acc881807c114e57f156cc9dd09950c4eed2
wilstermanz <4917@holbertonschool.com>
2022-06-22 11:30:53 -0700

    I broke it

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,30 +16,28 @@ dog_t *new_dog(char *name, float age, char *owner)
 	/* declarations */
 	dog_t *my_dog;
 	char *nameCopy, *ownerCopy;
-	
+
 	/* Check for NULL input */
 	if (name == NULL || owner == NULL)
 		return (NULL);
-	
+
 	/* Allocate space and check for errors */
 	my_dog = malloc(sizeof(*my_dog));
 	nameCopy = malloc(strlen((name) + 1) * sizeof(*name));
 	ownerCopy = malloc(strlen((owner) + 1) * sizeof(*owner));
 	if (my_dog == NULL || nameCopy == NULL || ownerCopy == NULL)
 	{
-		free(name);
-		free(owner);
 		free(nameCopy);
 		free(ownerCopy);
 		free(my_dog);
 		return (NULL);
 	}
-	
+
 	/* Set values */
 	strcpy(nameCopy, name);
 	strcpy(ownerCopy, owner);
-	my_dog->name = nameCopy;
+	my_dog->name = name;
 	my_dog->age = age;
-	my_dog->owner = ownerCopy;
+	my_dog->owner = owner;
 	return (my_dog);
 }
