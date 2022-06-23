ab7f77b98defbcdb203bdeed44db05f0b54e884c
wilstermanz <4917@holbertonschool.com>
2022-06-23 11:17:47 -0700

    Fixed edge case where input is NULL for all inputs

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,6 +15,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if (array == NULL || size <= 0 || action == NULL)
+		return;
+
 	for (i = 0; i < size; i++)
 		action(array[i]);
 }
