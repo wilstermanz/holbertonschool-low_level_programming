d6e979b8552ef05b50361116867345e33d51e811
wilstermanz <4917@holbertonschool.com>
2022-06-28 07:33:53 -0700

    Solutions for tasks 0 and 1

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Sums all its parameters
+ * @n: Number of parameters to sum
+ * @...: Parameters passed
+ * Return: Sum of parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum = 0;
+	
+	if (n == 0)
+		return (0);
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+
+	va_end(ap);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Prints numbers separated by separator
+ * @separator: string printed between numbers
+ * @n: number of ints to print
+ * @...: ints to print
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	if (separator == NULL)
+	{
+		for (i = 1; i < n; i++)
+		printf("%d", va_arg(ap, int));
+	}
+	else
+	{
+		for (i = 1; i < n; i++)
+			printf("%d%s", va_arg(ap, int), separator);
+	}
+	printf("%d\n", va_arg(ap, int));
+
+	va_end(ap);
+}
