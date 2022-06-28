768857dbcaffab0941d36ce80f94d4bf639d2ee9
wilstermanz <4917@holbertonschool.com>
2022-06-28 10:19:07 -0700

    Changed how sum is handled

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,13 +10,14 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	unsigned int i, sum = 0;
+	unsigned int i, sum;
 
 	if (n == 0)
 		return (0);
 
 	va_start(ap, n);
 
+	sum = 0;
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(ap, int);
