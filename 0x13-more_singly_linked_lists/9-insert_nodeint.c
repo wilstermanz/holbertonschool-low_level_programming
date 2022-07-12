d722739bcb9f569fea09f5d19b392f95fc533d59
wilstermanz <4917@holbertonschool.com>
2022-07-12 13:03:38 -0700

    Added case where idx = 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,6 +19,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		free(newNode);
 		return (NULL);
 	}
+	
+	if (idx == 0)
+	{
+		newNode->n = n;
+		newNode->next = ptr;
+		*head = newNode;
+		return (newNode);
+	}
 
 	for (i = 0; i < (idx - 1); i++)
 	{
