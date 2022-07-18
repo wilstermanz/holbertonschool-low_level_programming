384552897d0243513fcecfcce2b7618142243b41
wilstermanz <4917@holbertonschool.com>
2022-07-18 12:03:34 -0700

    Removed macros

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,21 +12,21 @@ int append_text_to_file(const char *filename, char *text_content)
 	int file, wr, size = 0;
 
 	if (filename == NULL)
-		return (EXIT_FAILURE);
+		return (-1);
 
 	if (text_content == NULL)
 		text_content = "";
 
 	file = open(filename, O_WRONLY | O_APPEND);
 	if (file == -1)
-		return (EXIT_FAILURE);
+		return (-1);
 
 	while (text_content[size])
 		size++;
 
 	wr = write(file, text_content, size);
 	if (wr == -1)
-		return (EXIT_FAILURE);
+		return (-1);
 
 	close(file);
 	return (1);
