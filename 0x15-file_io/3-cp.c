09c19f7f7181dc223ef5e8118465cbbc2cc8c702
wilstermanz <4917@holbertonschool.com>
2022-07-18 13:34:42 -0700

    Moved reading and writing to new function / fixed error code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,47 +9,67 @@
 
 int main(int argc, char **argv)
 {
-	char *file_from = argv[1], *file_to = argv[2], buff[1024];
-	int from_fd, to_fd, rd = 1024, wr;
+	char *file_from = argv[1], *file_to = argv[2];
+	int from_fd, to_fd;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+
 	from_fd = open(file_from, O_RDONLY);
 	if (from_fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
+
 	to_fd = open(file_to, O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
 	if (to_fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 		exit(99);
 	}
-	while (rd >= 1024)
-	{
-		rd = read(from_fd, buff, 1024);
-		if (rd == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-			closer(from_fd, to_fd);
-			exit(98);
-		}
-		wr = write(to_fd, buff, rd);
-		if (wr == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-			closer(from_fd, to_fd);
-			exit(98);
-		}
-	}
+
+	reader_writer(file_from, from_fd, file_to, to_fd);
+
 	closer(from_fd, to_fd);
 	return (0);
 }
 
+/**
+ * reader_writer - Reads from one file and writes to another
+ * file_from: name of file to read from
+ * from_fd: file descriptor of from file
+ * file_to: name of file to write to
+ * to_fd: file descriptor of to file
+ * Return: void
+ */
+
+void reader_writer(char *file_from, int from_fd, char *file_to, int to_fd)
+{
+	int rd, wr;
+	char buffer[1024];
+	
+	rd = 1024;
+	while (rd == 1024)
+	{
+		rd = read(from_fd, buffer, 1024);
+		if (rd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+			exit(98);
+		}
+
+		wr = write(to_fd, buffer, rd);
+		if (wr == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			exit(99);
+		}
+	}
+}
 
 /**
  * closer - closes open files
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,6 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
+void reader_writer(char *file_from, int from_fd, char *file_to, int to_fd);
 void closer(int file_from, int file_to);
 
 #endif /* MAIN_H */
