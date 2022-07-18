9e6a5b92b9e041daab373373480fb0999ccbd6e4
wilstermanz <4917@holbertonschool.com>
2022-07-18 10:54:17 -0700

    Freed alloc'd memory

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file;
 	size_t count;
-	char *buff = malloc(letters * sizeof(buff) + 1);
+	char *buff = malloc(letters * sizeof(buff));
 
 	if (buff == NULL || filename == NULL)
 	{
@@ -34,10 +34,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	read(file, buff, letters);
 
 	count = 0;
-	while (buff[count] != '\0')
+	while (buff[count] && count <= letters)
 		count++;
 
 	write(STDOUT_FILENO, buff, count);
+
+	free(buff);
 
 	return (count);
 }
